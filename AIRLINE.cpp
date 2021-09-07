d40df119f17cbd663e80356b539d90477e686d7b
Venkatesh1622 <78675743+Venkatesh1622@users.noreply.github.com>
2021-09-07 11:03:14 +0530

    Update AIRLINE.cpp

--- AIRLINE.cpp
+++ AIRLINE.cpp
@@ -4,5 +4,47 @@ using namespace std;
 int main()
 {
     
+   ll int t;
+   cin>>t;
+   while(t--)
+   {
+       int a[3];
+       int i,x=0,y;
+       for(i=0;i<3;i++)
+       cin>>a[i];
+       sort(a,a+3,greater<int>());
+      int d,e;
+      cin>>d>>e;
+      for(i=0;i<3;i++)
+      {
+          if(e>=a[i])
+          {
+          x++;
+          y=i;
+          break;
+          }
+      }
+      if(x==0)
+      {
+          cout<<"NO"<<endl;
+      }
+      else
+      {
+          int s=0;
+          for(i=0;i<3;i++)
+          {
+              if(i==y)
+              continue;
+              else
+              s+=a[i];
+          }
+          if(s<=d)
+          cout<<"YES"<<endl;
+          else
+          cout<<"NO"<<endl;
+      }
+      
+   }
+
 }
 
